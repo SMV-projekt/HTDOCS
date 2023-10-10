3f7d42b9f8c36db8bdfa58652622678a5b99f532
zamernikjan <jan.zamernik.17212@dijak.sc-celje.si>
2023-10-10 08:46:44 +0200

    popravki

--- registracija.php
+++ registracija.php
@@ -2,22 +2,22 @@
 include 'database.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['email'];
+    $email = $_POST['Email'];
     $ime_dijaka = $_POST['ime_dijaka'];
     $priimek_dijaka = $_POST['priimek_dijaka'];
-    $geslo = $_POST['geslo'];
-    $letnik = $_POST['letnik'];
-    $spol = $_POST['spol'];
+    $geslo = $_POST['Geslo'];
+    $letnik = $_POST['Letnik'];
+    $spol = $_POST['Spol'];
 
-    // Validate the form data
+   
     if (empty($email) || empty($ime_dijaka) || empty($priimek_dijaka) || empty($geslo) || empty($letnik) || empty($spol)) {
         echo "Please fill in all the fields.";
     } else {
-        // Hash the password
+        
         $hashedPassword = password_hash($geslo, PASSWORD_DEFAULT);
 
-        // Check if the user already exists
-        $sql = "SELECT * FROM dijak WHERE `E-mail` = ?";
+        
+        $sql = "SELECT * FROM dijak WHERE Email = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -26,8 +26,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($result->num_rows > 0) {
             echo "User already exists!";
         } else {
-            // Insert the user into the dijak table
-            $sql = "INSERT INTO dijak (ime_dijaka, priimek_dijaka, `E-mail`, geslo, letnik, spol)
+            
+            $sql = "INSERT INTO dijak (ime_dijaka, priimek_dijaka, Email, Geslo, Letnik, Spol)
                     VALUES (?, ?, ?, ?, ?, ?)";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("ssssss", $ime_dijaka, $priimek_dijaka, $email, $hashedPassword, $letnik, $spol);
@@ -43,6 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     exit('Invalid request!');
 }
 
-// Close the database connection
+
 mysqli_close($conn);
 ?>
