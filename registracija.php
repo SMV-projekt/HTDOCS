df791e61ab5975bfd64dd38be86a477e8f5ec5e4
zamernikjan <jan.zamernik.17212@dijak.sc-celje.si>
2023-10-11 17:41:21 +0200

    Delujoča registracija
    HTML in PHP sem dal v en PHP

--- registracija.php
+++ registracija.php
@@ -2,22 +2,20 @@
 include 'database.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['Email'];
+    $email = $_POST['email'];
     $ime_dijaka = $_POST['ime_dijaka'];
     $priimek_dijaka = $_POST['priimek_dijaka'];
-    $geslo = $_POST['Geslo'];
-    $letnik = $_POST['Letnik'];
-    $spol = $_POST['Spol']; 
+    $geslo = $_POST['geslo'];
+    $letnik = $_POST['letnik'];
+    $spol = $_POST['spol'];
 
-   
     if (empty($email) || empty($ime_dijaka) || empty($priimek_dijaka) || empty($geslo) || empty($letnik) || empty($spol)) {
         echo "Please fill in all the fields.";
     } else {
-        
         $hashedPassword = password_hash($geslo, PASSWORD_DEFAULT);
 
         
-        $sql = "SELECT * FROM dijak WHERE Email = ?";
+        $sql = "SELECT * FROM dijak WHERE `E-mail` = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -27,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "User already exists!";
         } else {
             
-            $sql = "INSERT INTO dijak (ime_dijaka, priimek_dijaka, Email, Geslo, Letnik, Spol)
+            $sql = "INSERT INTO dijak (ime_dijaka, priimek_dijaka, `E-mail`, geslo, letnik, spol)
                     VALUES (?, ?, ?, ?, ?, ?)";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("ssssss", $ime_dijaka, $priimek_dijaka, $email, $hashedPassword, $letnik, $spol);
@@ -40,9 +38,61 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 } else {
-    exit('Invalid request!');
-}
+    
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Registracija</title>
+        <link rel="stylesheet" type="text/css" href="registracija.css" />
+        <script type="text/javascript">
+            window.onload = function () {
+                document.SignUp.SignUpBtn.disabled = true;
 
+                document.SignUp.addEventListener("change", () => {
+                    document.getElementById('SignUpBtn').disabled = !checkSignUp()
+                });
+            }
+
+            function checkSignUp() {
+                if (document.SignUp.email.value == '') return false;
+                if (document.SignUp.ime_dijaka.value == '') return false;
+                if (document.SignUp.priimek_dijaka.value == '') return false;
+                if (document.SignUp.geslo.value == '') return false;
+
+                return true;
+            }
+        </script>
+    </head>
+    <body>
+        <header></header>
+        <h1>Registracija</h1>
+        <p>Že imate račun? <a href="prijava.html">Prijava</a></p>
+        <form name="SignUp" id="registracija" action="registracija.php" method="post">
+            <input type="email" name="email" id="email" placeholder="Email" required /><br />
+            <input type="text" name="ime_dijaka" id="ime_dijaka" placeholder="Ime" required /><br />
+            <input type="text" name="priimek_dijaka" id="priimek_dijaka" placeholder="Priimek" required /><br />
+            <input type="password" name="geslo" id="geslo" placeholder="Geslo" required /><br />
+            <label for="letnik">Letnik:</label>
+            <select name="letnik" id="letnik">
+                <option value="1">1. letnik</option>
+                <option value="2">2. letnik</option>
+                <option value="3">3. letnik</option>
+                <option value="4">4. letnik</option>
+            </select><br />
+            <label for="spol">Spol:</label>
+            <select name="spol" id="spol" required>
+                <option value="">Izberite spol</option>
+                <option value="M">Moški</option>
+                <option value="Ž">Ženski</option>
+            </select><br />
+            <button type="submit" id="SignUpBtn">Registracija</button>
+        </form>
+    </body>
+    </html>
+    <?php
+}
 
 mysqli_close($conn);
 ?>
